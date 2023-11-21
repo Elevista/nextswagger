e97c020a8ec09716de487cbeb6d445eb093507cb
Elevista <sunnyholic@sunnyholic.com>
2023-11-21 15:45:10 +0900

    tswagger version 1

--- src/TemplateNext.ts
+++ src/TemplateNext.ts
@@ -3,9 +3,6 @@ import { NexTSwaggerOptions as Options } from './index'
 export type NextTemplateOptions = Options & { relTypePath: string }
 interface NextTemplate extends TemplateCommon { hasAxiosConfig: boolean }
 
-function axiosArrowFn (args: string, returnType: string, methodType: string, params: string) {
-  return `(${args}): $R<${returnType}> => _('${methodType}', ${params})`
-}
 function pluginTemplate (this: NextTemplate, { object }: { object: string }) {
   const { importTypes, multipart, noInspect, options: { pluginName }, hasAxiosConfig, exportCode } = this
 
@@ -17,10 +14,14 @@ const { publicRuntimeConfig } = getConfig()
   const axiosConfig = hasAxiosConfig ? `[publicRuntimeConfig?.nextswagger].flat().find(x => x?.pluginName === '${pluginName}')?.axiosConfig || {}` : '{}'
   return `
 ${noInspect}
-import Axios, { AxiosStatic, AxiosRequestConfig, AxiosResponse } from 'axios'
+import Axios, { AxiosStatic, AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios'
 ${importConfig}${importTypes}
 export interface $customExtendResponse {}
-type $R<T> = Promise<T> & { readonly response: Promise<AxiosResponse<T> & $customExtendResponse> }
+interface PromiseEt<T, E> extends Promise<T> {
+  then<R1 = T, R2 = never>(onfulfilled?: ((value: T) => R1 | PromiseLike<R1>) | undefined | null, onrejected?: ((reason: E) => R2 | PromiseLike<R2>) | undefined | null): Promise<R1 | R2>;
+  catch<R1 = never>(onrejected?: ((reason: E) => R1 | PromiseLike<R1>) | undefined | null): Promise<T | R1>;
+}
+type $R<T, E> = PromiseEt<T, E> & { readonly response: PromiseEt<AxiosResponse<T> & $customExtendResponse, AxiosError<E>> }
 export const $axiosConfig: Required<Parameters<AxiosStatic['create']>>[0] = ${axiosConfig}
 const $ep = (_: any) => (${object})
 ${exportCode}
@@ -33,7 +34,6 @@ export class V2 extends V2T {
     super(spec, options)
     this.hasAxiosConfig = !!options.axiosConfig
     this.pluginTemplate = pluginTemplate
-    this.axiosArrowFn = axiosArrowFn
   }
 }
 
@@ -43,6 +43,5 @@ export class V3 extends V3T {
     super(spec, options)
     this.hasAxiosConfig = !!options.axiosConfig
     this.pluginTemplate = pluginTemplate
-    this.axiosArrowFn = axiosArrowFn
   }
 }
